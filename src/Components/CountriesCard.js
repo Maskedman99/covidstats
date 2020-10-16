af3786facbd6caac072f503bff58efab8b9ac192
Rohit <rohitmpaul@gmail.com>
2020-10-16 16:00:41 +0530

    add keys to the top list, and remove an unwanted View

--- src/Components/CountriesCard.js
+++ src/Components/CountriesCard.js
@@ -6,32 +6,46 @@ import {formatNumber, shortenCountryName} from '../Logic/misc';
 
 const CountriesCard = ({topCountries}) => {
   return (
-    <Card>
-      <View style={styles.container}>
-        <View>
-          <ThemedText style={styles.header}>Country</ThemedText>
-          {topCountries.map((item, id) => {
-            return <ThemedText style={styles.data}>{shortenCountryName(item.country)}</ThemedText>;
-          })}
-        </View>
-        <View>
-          <ThemedText style={styles.header}>Confirmed</ThemedText>
-          {topCountries.map((item, id) => {
-            return <ThemedText style={styles.data}>{formatNumber(item.cases)}</ThemedText>;
-          })}
-        </View>
-        <View>
-          <ThemedText style={styles.header}>Active</ThemedText>
-          {topCountries.map((item, id) => {
-            return <ThemedText style={styles.data}>{formatNumber(item.active)}</ThemedText>;
-          })}
-        </View>
-        <View>
-          <ThemedText style={styles.header}>Deaths</ThemedText>
-          {topCountries.map((item, id) => {
-            return <ThemedText style={styles.data}>{formatNumber(item.deaths)}</ThemedText>;
-          })}
-        </View>
+    <Card style={styles.container}>
+      <View>
+        <ThemedText style={styles.header}>Country</ThemedText>
+        {topCountries.map((item, id) => {
+          return (
+            <ThemedText style={styles.data} key={id}>
+              {shortenCountryName(item.country)}
+            </ThemedText>
+          );
+        })}
+      </View>
+      <View>
+        <ThemedText style={styles.header}>Confirmed</ThemedText>
+        {topCountries.map((item, id) => {
+          return (
+            <ThemedText style={styles.data} key={id}>
+              {formatNumber(item.cases)}
+            </ThemedText>
+          );
+        })}
+      </View>
+      <View>
+        <ThemedText style={styles.header}>Active</ThemedText>
+        {topCountries.map((item, id) => {
+          return (
+            <ThemedText style={styles.data} key={id}>
+              {formatNumber(item.active)}
+            </ThemedText>
+          );
+        })}
+      </View>
+      <View>
+        <ThemedText style={styles.header}>Deaths</ThemedText>
+        {topCountries.map((item, id) => {
+          return (
+            <ThemedText style={styles.data} key={id}>
+              {formatNumber(item.deaths)}
+            </ThemedText>
+          );
+        })}
       </View>
     </Card>
   );
